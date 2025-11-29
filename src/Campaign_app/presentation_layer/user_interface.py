6e4b66e51be86668e6d3ef5aa7d6f1d030559015
Drew Miller <drewmiller554@gmail.com>
2025-11-28 20:55:42 -0500

    Finalized edits for User interface

--- src/Campaign_app/presentation_layer/user_interface.py
+++ src/Campaign_app/presentation_layer/user_interface.py
@@ -1,6 +1,8 @@
 """Implements the applicatin user interface."""
+
 from campaign_app.application_base import ApplicationBase
 from campaign_app.service_layer.app_services import AppServices
+from prettytable import PrettyTable
 import sys
 
 class UserInterface(ApplicationBase):
@@ -12,20 +14,75 @@ class UserInterface(ApplicationBase):
         super().__init__(subclass_name=self.__class__.__name__, 
 				   logfile_prefix_name=self.META["log_prefix"])
         self.app_services = AppServices(config)
+    
+    
+    # Public Methods
     def display_menu(self) ->None:
         """Display the menu"""
-        print(f"\t\tCampaign Channel Application Menu")
+        print(f"\n\n\t\tCampaign Channel Application Menu")
         print()
         print(f"\t1. List Campaigns")
         print(f"\t2. List Channel")
         print(f"\t3. Add Campaign")
-        print(f"\t4 Record Campaign")
+        print(f"\t4. Record Campaign")
         print(f"\t5. Add Channel")
-        print(f"t\6. Exit")
+        print(f"\t6. Exit")
         print()
+    
+
+    def process_menu_choice(self)->None:
+        """Proccess choices of menu"""
+        menu_choice = input("\tMenu Choice: ")
+
+        match menu_choice[0]:
+            case '1': self.list_campaigns()
+            case '2': self.list_channel()
+            case '3': self.add_channel()
+            case '4': self.record_campaign()
+            case '5': self.add_channel()
+            case '6': sys.exit()
+            case _: print(f"Invalid Menu Choice {menu_choice[0]}")
+
+    def list_campaigns(self)->None:
+        """Lists campaigns"""
+        campaigns = self.app_services.get_all_campaigns()
+        campaign_table = PrettyTable()
+        campaign_table.field_names =['id','Campaign Name','Start Date','End Date',
+                                     'Company','Campaign Category','Budget','Revenue',]
+        channel_table = PrettyTable()
+        channel_table.field_names = ['Channel_Name']
+        channel_table.align = 'l'
+        for campaign in campaigns:
+            for channel in campaign.channel:
+                channel_table.add_row([channel.ChannelName])
+            campaign_table.add_row([campaign.idCampaign, campaign.Campaign_Name,
+                                    campaign.StartDate, campaign.EndDate,
+                                    campaign.idCompany, campaign.idCampaign_Category,
+                                    campaign.Budget, campaign.Revenue, campaign.NetProfit,
+                                    channel_table.get_string()])
+            campaign_table.add_divider()
+            channel_table.clear_rows()
+        print(campaign_table)
 
 
+    def list_channel(self)->None:
+        """Lists channel"""
+        print("list_channel() method stub called....")
+    
 
+    def add_campaign(self)->None:
+        """add campaign"""
+        print("add_campaign() method stub called....")
+    
+    def record_campaign(self)->None:
+        """record campaign"""
+        print("record_campaign() method stub called....")
+    
+    def add_channel(self)->None:
+        """Add channel"""
+        print("add_channel() method stub called....")
 
     def start(self) ->None:
-        self.display_menu()
+        while True:
+            self.display_menu()
+            self.process_menu_choice
